b68f36d8cf489909c9a211a7050da7c6b1f77332
hrodmn <henry.rodman@gmail.com>
2025-05-05 20:44:28 -0500

    temporarily create a new API Gateway endpoint

--- cdk/PgStacInfra.ts
+++ cdk/PgStacInfra.ts
@@ -9,6 +9,8 @@ import {
   aws_cloudfront as cloudfront,
   aws_cloudfront_origins as origins,
   aws_cloudwatch as cloudwatch,
+  aws_apigatewayv2_integrations,
+  CfnOutput,
 } from "aws-cdk-lib";
 import { Aws, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
@@ -204,6 +206,28 @@ export class PgStacInfra extends Stack {
       ec2.Port.tcp(5432),
       "allow connections from titiler",
     );
+
+    const altTitilerApi = new apigatewayv2.HttpApi(
+      this,
+      `${Stack.of(this).stackName}-titiler-pgstac-alt-api`,
+      {
+        defaultIntegration:
+          new aws_apigatewayv2_integrations.HttpLambdaIntegration(
+            "integration",
+            titilerPgstacFnVersion,
+          ),
+      },
+    );
+
+    const apiUrl = altTitilerApi.url;
+    if (typeof apiUrl !== "string") {
+      throw new Error("Expected API URL to be a string");
+    }
+
+    new CfnOutput(this, "alt-titiler-pgstac-api-output", {
+      exportName: `${Stack.of(this).stackName}-alt-titiler-pgstac-url`,
+      value: apiUrl,
+    });
 
     // API logging dashboard
 
